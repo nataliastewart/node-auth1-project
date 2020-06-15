89c6cf244ab730d09e20f2c8962abf9a9b3f5d96
Natalia <nataliamirandac@hotmail.com>
2020-06-15 16:47:42 -0400

    auth register is working on postman - brought it back a password

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 //import routers
 const usersRouter = require("../users/users-router.js");
-
+const authRouter = require("../auth/auth-router.js");
 //server
 const server = express();
 
@@ -11,5 +11,5 @@ server.use(express.json());
 
 //routers
 server.use("/api/users", usersRouter);
-
+server.use("/api/auth", authRouter);
 module.exports = server;
---
+++ auth/auth-router.js
@@ -0,0 +1,22 @@
+const bcryptjs = require("bcryptjs");
+
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  // validate the body, to make sure there is a username and password.
+  const { username, password } = req.body;
+
+  // hash user passwordjs (install bcryptjs)
+  const rounds = process.env.HASH_ROUNDS || 8; // change to a higher number in production
+  const hash = bcryptjs.hashSync(password, rounds);
+
+  Users.add({ username, password: hash })
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => res.send(err));
+});
+
+module.exports = router;
